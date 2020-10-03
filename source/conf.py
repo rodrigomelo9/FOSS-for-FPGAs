d5e2ab33a04c22489741a8e02cecbdd0d7240212
Rodrigo Alejandro Melo <rodrigomelo9@gmail.com>
2020-10-03 16:30:18 -0300

    Modified to use the BTD theme
    Signed-off-by: Rodrigo Alejandro Melo <rodrigomelo9@gmail.com>

--- source/conf.py
+++ source/conf.py
@@ -1,7 +1,22 @@
+from pathlib import Path
+
 project = 'Getting started with FOSS for FPGAs'
 copyright = '2020, Rodrigo A. Melo, licensed under CC BY 4.0'
 author = 'Rodrigo A. Melo (RAM)'
 release = 'master'
-html_theme = 'sphinx_rtd_theme'
+
 extensions = ['recommonmark']
-exclude_patterns = ['README.md']
+
+html_theme_options = {
+    'logo_only': True,
+    'home_breadcrumbs': False,
+    'vcs_pageview_mode': 'blob',
+}
+
+html_context = {}
+ctx = Path(__file__).resolve().parent / 'context.json'
+if ctx.is_file():
+    html_context.update(loads(ctx.open('r').read()))
+
+html_theme_path = ["."]
+html_theme = "_theme"
