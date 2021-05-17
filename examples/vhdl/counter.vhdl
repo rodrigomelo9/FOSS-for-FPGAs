b3e94faae4e0a707a016d0bd8d82217e10509e37
Rodrigo A. Melo <rodrigomelo9@gmail.com>
2021-05-17 18:39:40 -0300

    Simplified the counter VHDL example

--- examples/vhdl/counter.vhdl
+++ examples/vhdl/counter.vhdl
@@ -3,16 +3,18 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
 entity counter is
+    generic (
+        WIDTH  : positive := 4
+    );
     port (
         clk_i : in  std_logic;
         rst_i : in  std_logic;
-        cnt_o : out std_logic_vector(3 downto 0)
+        cnt_o : out std_logic_vector(WIDTH-1 downto 0)
     );
 end entity counter;
 
 architecture RTL of counter is
-    constant MODULE : positive := 12;
-    signal cnt      : unsigned(3 downto 0);
+    signal cnt : unsigned(WIDTH-1 downto 0);
 begin
     do_counter : process (clk_i)
     begin
@@ -20,11 +22,7 @@ begin
             if rst_i = '1' then
                 cnt <= (others => '0');
             else
-                if cnt < MODULE-1 then
-                    cnt <= cnt + 1;
-                else
-                    cnt <= (others => '0');
-                end if;
+                cnt <= cnt + 1;
             end if;
         end if;
     end process do_counter;
