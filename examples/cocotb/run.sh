61d389dc11c8aa0b577fa8ee0ee2a6dc1bf175ef
Rodrigo A. Melo <rodrigomelo9@gmail.com>
2022-03-08 19:09:41 -0300

    cocotb: added a TestFactory example

--- examples/cocotb/run.sh
+++ examples/cocotb/run.sh
@@ -1,8 +1,11 @@
 DOCKER="docker run --rm -v $PWD/..:$PWD/.. -w $PWD"
 IMAGE=hdlc/sim:osvb
 
-$DOCKER $IMAGE make SIM=icarus VERILOG_SOURCES=../vlog/counter.v
+$DOCKER $IMAGE make -C counter SIM=icarus
 
-$DOCKER $IMAGE make SIM=ghdl VHDL_SOURCES=../vhdl/counter.vhdl VERILOG_SOURCES=
+$DOCKER $IMAGE make -C counter SIM=ghdl VHDL_SOURCES=../../vhdl/counter.vhdl VERILOG_SOURCES=
 
-$DOCKER $IMAGE make clean
+$DOCKER $IMAGE make -C factory
+
+$DOCKER $IMAGE make -C counter clean
+$DOCKER $IMAGE make -C factory clean
