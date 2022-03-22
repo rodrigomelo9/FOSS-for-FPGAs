ea4a5f94063d823d35ff64c352394030a95dee8c
Rodrigo A. Melo <rodrigomelo9@gmail.com>
2022-03-22 11:04:55 -0300

    cocotb: added examples testend and timeout

--- examples/cocotb/run.sh
+++ examples/cocotb/run.sh
@@ -1,6 +1,8 @@
 DOCKER="docker run --rm -v $PWD/..:$PWD/.. -w $PWD"
 IMAGE=hdlc/sim:osvb
 
+docker pull $IMAGE
+
 $DOCKER $IMAGE make -C counter SIM=icarus
 
 $DOCKER $IMAGE make -C counter SIM=ghdl VHDL_SOURCES=../../vhdl/counter.vhdl VERILOG_SOURCES=
@@ -10,6 +12,12 @@ $DOCKER $IMAGE make -C factory
 $DOCKER $IMAGE make -C logging
 $DOCKER $IMAGE make -C logging COCOTB_LOG_LEVEL=debug
 
+$DOCKER $IMAGE make -C testend
+
+$DOCKER $IMAGE make -C timeout
+
 $DOCKER $IMAGE make -C counter clean
 $DOCKER $IMAGE make -C factory clean
 $DOCKER $IMAGE make -C logging clean
+$DOCKER $IMAGE make -C testend clean
+$DOCKER $IMAGE make -C timeout clean
